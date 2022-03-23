1f645bd2a181d177be1eff2f71e94f31114a38ec
Aakash kumar Agrawal <akagrawal4499@gmail.com>
2022-03-23 14:41:44 +0530

    2nd Commit

--- Test.py
+++ Test.py
@@ -10,7 +10,7 @@ def testFunction():
 	request_data = request.get_json()
 	print(request_data) 
 
-	return jsonify({"Output":"THIS IS a 2nd MESSAGE"})
+	return jsonify({"Output":"THIS is the 3rd message"})
 
 if __name__ == '__main__':
 	#serving the application using waitress
