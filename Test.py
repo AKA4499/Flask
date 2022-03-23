63f235f7dff6210ba828c1fada81e6fd31a2559c
AKA4499 <39817253+AKA4499@users.noreply.github.com>
2022-03-23 13:32:06 +0530

    Add files via upload

---
+++ Test.py
@@ -0,0 +1,18 @@
+from flask import Flask, request, jsonify
+from waitress import serve
+#from PIL import Image 
+#creating flask app
+app = Flask(__name__)
+
+@app.route('/', methods = ['GET'])
+def testFunction():
+	#request from the front end
+	request_data = request.get_json()
+	print(request_data) 
+
+	return jsonify({"Output":"THIS IS MESSAGE"})
+
+if __name__ == '__main__':
+	#serving the application using waitress
+	# serve(app, host="127.0.0.1", port="8001", threads = 1)
+	app.run(host="127.0.0.1", port=8001, debug = True)
